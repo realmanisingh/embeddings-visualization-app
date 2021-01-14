0fe1d47de4dab14879da7db627e2e7a714f72192
Mani <realmanisingh@gmail.com>
2021-01-13 20:51:32 -0600

    Created function for lowercasing the text

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -7,13 +7,25 @@ class Preprocess:
     Attributes:
         text (str): the text that will be preprocessed.
     """
-    def __init__(self, text):
+    def __init__(self, text: str):
         """
         The constructor for the Preprocess class.
-     
-        param text (str): the text that will be preprocessed
+        
+        Parameters:
+            text (str): the text that will be preprocessed
         """
         self.text = text
     
+    def lowercase(self) -> str:
+        """
+        Lowercases the text
+        
+        Returns:
+            lower_text (str): the lowercased text
+        """
+        lower_text = self.text.lower()
+        
+        return lower_text
+        
     
     

f524b0854251a0063c09c781dcca2a052fe57e64
Mani <realmanisingh@gmail.com>
2021-02-04 19:26:10 -0500

    Started function for creating word2vec model

--- nlp_models/word2vec.py
+++ nlp_models/word2vec.py
@@ -1,2 +1,21 @@
 from gensim.models import Word2Vec
+#from preprocessing.preprocess import Preprocess
+from typing import List
+from pandas import DataFrame
 
+def create_word2Vec(tokens: List[str]) -> DataFrame:
+    """
+    Creates an instance of a Word2Vec model and creates embeddings
+    for the input text
+    
+    Parameters:
+        tokens (str): List of tokens for some text
+    """
+    
+    model = Word2Vec(sentences=tokens, size=60, sg=1, min_count=1)
+    
+    return model
+    
+
+test = create_word2Vec(["hello there", "I see you"])
+print(list(test.wv.vocab))
